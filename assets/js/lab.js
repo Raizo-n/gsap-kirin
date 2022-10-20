8993d1c023083b024daf602ea9d516614b8ae5ce
“Raizo-n” <sanpincha0826@gmail.com>
2022-10-19 23:31:07 -0700

    added the model answer

--- assets/js/lab.js
+++ assets/js/lab.js
@@ -2,13 +2,27 @@ window.addEventListener('load', revealAnimation)
 
 function revealAnimation(){
 
-    const Timeline = gsap.timeline()
+    const Timeline = gsap.timeline({
+        defaults: {
+            duration: 1,
+            autoAlpha: 0,
+            ease: "power4.out"
+        }
+    });
 
     Timeline
-    .from('.nav', {autoAlpha: 0, x: -50, duration: 1.5, ease: Power4})
-    .from('.title', {autoAlpha: 0, y: 50}, '-=0.2')
-    .from('.tagline', {autoAlpha: 0, y: 50})
-    .from('.desc', {autoAlpha: 0, y: 50}, '-=0.1')
-    .from('img', {autoAlpha: 0, y: -500, duration: 2})
+    // My answer
+    // .from('.nav', {autoAlpha: 0, x: -50, duration: 1.5, ease: Power4})
+    // .from('.title', {autoAlpha: 0, y: 50}, '-=0.2')
+    // .from('.tagline', {autoAlpha: 0, y: 50})
+    // .from('.desc', {autoAlpha: 0, y: 50}, '-=0.1')
+    // .from('img', {autoAlpha: 0, y: -500, duration: 2})
+
+    .from('.logo', {x: -20})
+    .from('.menu-links ul li', {x: -20, stagger: 0.08})
+    .from('.title', {y: 20}, '-=0.4')
+    .from('.tagline', {y: 20}, '-=0.7')
+    .from('.desc', {y: -20}, '<')
+    .from('.beer', {y: -800})
 
 }
